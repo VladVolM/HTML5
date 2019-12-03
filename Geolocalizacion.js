9622f7eb07896ed46b69d15073383e84254cffc0
alumno <alumno@pc18.iris.majuelo.es>
2019-12-03 16:14:22 +0100

    Intento de añadir geolocalizacion

---
+++ Geolocalizacion.js
@@ -0,0 +1,35 @@
+var x = document.getElementById("geo");
+function getLocation() {
+
+	window.alert(x);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+	window.alert(2);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+	window.alert(3);
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+
+}
